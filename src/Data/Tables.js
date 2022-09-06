f20d5c86a5f65c53216f23144e013cdcf8660d52
mwstarbuck <mwstarbuck@gmail.com>
2022-09-06 11:19:22 -0500

    Added table 2-1

---
+++ src/Data/Tables.js
@@ -0,0 +1,57 @@
+export const TABLE_2_1 = [
+  {
+    id: 1,
+    campaign: 1,
+    timePeriod: '8/1942 - 4/1943',
+    aircraft: ['B-17F'],
+    base: '8th Airforce (England)',
+    missions: 25,
+    altMissions: null
+  },
+  {
+    id: 2,
+    campaign: 2,
+    timePeriod: '5/1943 - 11/1943',
+    aircraft: ['B-17F', 'B-24D, YB-40'],
+    base: '8th Airforce (England)',
+    missions: 25,
+    altMissions: null
+  },
+  {
+    id: 3,
+    campaign: 3,
+    timePeriod: '12/1943 - 5/1944, *6/1944 - 11/1944',
+    aircraft: ['B-17F', 'B-17 G', 'B-24D', 'B-24J', 'YB-40'],
+    base: '8th Airforce (England)',
+    missions: 25,
+    altMissions: 30
+  },
+  {
+    id: 4,
+    campaign: 4,
+    timePeriod: '11/1943 - 5/1944',
+    aircraft: ['B-17F', 'B-17G', 'B-24D', 'B-24J'],
+    base: '15th Airforce (Italy)',
+    missions: 30,
+    altMissions: null
+  },
+  {
+    id: 5,
+    campaign: 5,
+    timePeriod: '6/1944 - 11/1944, *12/1944 - 4/1945',
+    aircraft: ['B-17G', 'B-24J'],
+    base: '15th Airforce (Italy)',
+    missions: 35,
+    altMissions: 50
+  },
+  {
+    id: 6,
+    campaign: 6,
+    timePeriod: '12/1944 - 4/1945',
+    aircraft: ['B-17G', 'B-24J'],
+    base: '8th Airforce (England)',
+    missions: 35,
+    altMissions: null
+  },
+
+]
---
+++ src/Game/GamePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GamePage = () => {
+
+  return <h1>Target for Today Helper</h1>
+}
+
+export default GamePage;
