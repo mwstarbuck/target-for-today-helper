d05cbc52eddd7d41056faff38717cf3f74408d0f
Mike Starbuck <miglas9@gmail.com>
2022-09-08 19:09:17 -0500

    added next step and go back buttons

--- src/Game/GamePage.js
+++ src/Game/GamePage.js
@@ -12,17 +12,17 @@ const GamePage = () => {
     const campaign = (TABLE_2_1.find(c => c.id === result));
     setCampaign(campaign);
   }
-  console.log(campaign);
   return <><h1 style={{ opacity: 0.7 }}>Target for Today Helper</h1>
 
     {!showRoller && <button style={{ fontFamily: 'Courier', opacity: 0.7 }} onClick={() => setShowRoller(!showRoller)}>Start Game</button>}
     {showRoller && <button style={{ fontFamily: 'Courier' }} onClick={handleRoll}>Roll for campaign</button>}
 
-    {campaign ? <span style={{ opacity: 0.7 }}> <h3>CAMPAIGN #: {campaign.campaign}</h3>
+    {campaign && <span style={{ opacity: 0.7 }}> <h3>CAMPAIGN #: {campaign.campaign}</h3>
       <h4>PERIOD: {campaign.timePeriod}</h4>
       <h4>AIRCRAFT: {campaign.aircraft}</h4>
-      <h5>BASING: {campaign.base}</h5>
-      <h5>MISSIONS: {campaign.missions}</h5> </span> : <diz style={{ width: '100%', height: '100%' }}></diz>}
+      <h5>BASING: <span style={{ textDecoration: 'underline' }}>{campaign.base}</span></h5>
+      <h5>MISSIONS: {campaign.missions}</h5>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}><button onClick={() => setStep(step + 1)}>Next Step</button><button>Cancel/Go Back</button></div></span>}
   </>
 }
 
