c57493b5dc0bc6f46c8c0b43aa356a8836b35375
mwstarbuck <mwstarbuck@gmail.com>
2022-12-01 18:01:41 -0600

    fighter

--- src/Game/PageComponents/Combat/Fighter.js
+++ src/Game/PageComponents/Combat/Fighter.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { fighters } from '../../../Data/Options';
 
 const Fighter = () => {
   const [type, setType] = useState(null);
   const [skill, setSkill] = useState('average');
   const [status, setStatus] = useState(null);
-  const [angle, setAngle] = useState();
+  const [angle, setAngle] = useState(null);
+  const [attacks, setAttack] = useState(null)
 
   return <div>Fighter</div>
 }
