3536f149ed5f5c6e44abcc221aaa8a2f65c9a312
Mike Starbuck <miglas9@gmail.com>
2022-12-31 11:53:41 -0600

    fix skill table

--- src/Modals/GFAModal/GFAssignSkill.js
+++ src/Modals/GFAModal/GFAssignSkill.js
@@ -7,8 +7,16 @@ const GFAssignSkill = (props) => {
 
   return <>
     <Row>
-      <Col span={24}>
+      <Col span={12}>
         <FighterSkill />
+      </Col>
+      <Col span={12}>
+        <div>
+          <div style={{ textAlign: 'center', marginBottom: -20 }}><p style={{ fontSize: 19, fontWeight: 600 }}>German Fighter Skill</p><p style={{ fontSize: 16, fontWeight: 600 }}>(Roll 1D6)</p></div>
+          <Popover trigger='click' placement='left' content={<><div style={{ fontWeight: 600, fontSize: 18 }}>German Fighter Skill Notes</div><img src={require('../../Images/TableNotes/5-5A-note.png')} style={{ opacity: 0.8, paddingTop: 10, alignSelf: 'baseline' }} /></>} ><a style={{ cursor: 'pointer', textAlign: 'right' }}>See Table Notes</a>
+          </Popover>
+          <div style={{ alignItems: 'center' }}><img src={require('../../Images/Tables/5-5A.png')} style={{ opacity: opacity || 0.6, paddingTop: 10, alignSelf: 'baseline' }} /></div>
+        </div>
       </Col>
     </Row>
   </>
