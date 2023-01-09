b1bec98af68836a595e2248c4bb0ada6dfb59d46
mwstarbuck <mwstarbuck@gmail.com>
2023-01-09 17:08:45 -0600

    test save

--- src/Game/Test/Test.js
+++ src/Game/Test/Test.js
@@ -3,23 +3,28 @@ import NumberAnglesAndLevels from './NumberAnglesAndLevels';
 import { Steps, Button } from 'antd';
 import GameContext from '../GameContext';
 
-
-
-
 const Test = () => {
   const ctx = useContext(GameContext);
-
-  const onSave = () => {
+  let fileHandle;
+  
+  const onSave = async () => {
     const fileData = JSON.stringify(ctx, null, '\t');
-    const blob = new Blob([fileData], {type: 'text/plain'});
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.download = 'tft-saves/tftsave01';
-    link.href = url;
-    link.click();
-  }
+    const blob = new Blob([fileData], { type: 'text/plain' });
+    const newHandle = await window.showSaveFilePicker();
+    const writableStream = await newHandle.createWritable();
+    await writableStream.write(blob);
+    await writableStream.close();
 
+    // const blob = new Blob([fileData], {type: 'text/plain'});
+    // const url = URL.createObjectURL(blob);
+    // const link = document.createElement('a');
+    // link.download = 'tft-saves/tftsave01';
+    // link.href = url;
+    // link.click();
+  }
+  
   const onLoad = () => {
+
     const saveData = JSON.parse();
     // const url = URL.createObjectURL(blob);
     // const link = document.createElement('a');
